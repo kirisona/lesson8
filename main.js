7f62687f09394807a4bfd46e02949cf86af739ea
Sofia Varich <kirisona@gmail.com>
2019-09-20 01:00:12 +0300

    all

--- main.js
+++ main.js
@@ -159,4 +159,66 @@ console.log(getElementHeight());
 
 console.log('arrow function');
 
-/** */
+/**Переделать функцию с использованием функции-стрелки (в методе reduce тоже использовать arrow function):
+
+function sum() {
+  const params = Array.prototype.slice.call(arguments);
+
+  if (!params.length) return 0;
+
+  return params.reduce(function (prev, next) { return prev + next; });
+}
+
+sum(1, 2, 3, 4); // 10
+sum(); // 0 */
+
+let newSum = (...args) => {
+  if (!args.length) return 0;
+  return args.reduce((prev, next) => prev + next);
+};
+console.log(newSum(1, 2, 3, 4));
+
+/**Используя rest оператор и деструктуризацию, создать функцию, которая принимает любое количество аргументов и возвращает объект, содержащий первый аргумент и массив из остатка:
+
+func(‘a’, ‘b’, ‘c’, ‘d’) → 
+{
+  first: ‘a’,
+  other: [‘b’, ‘c’, ‘d’]
+} */
+
+function destructFunc(arg1, ...args) {
+  return arg1, args;
+}
+
+console.log(destructFunc(1, 2, 3, 4));
+
+/**Организовать функцию getInfo, которая принимает объект вида
+{ name: ...,  info: { employees: [...], partners: [ … ]  } }
+и выводит в консоль имя (если имени нет, показывать ‘Unknown’) и первые две компании из массива partners:
+
+const organisation = {  
+  name: 'Google',   
+  info: { employees: [‘Vlad’, ‘Olga’], partners: ['Microsoft', 'Facebook', 'Xing']   } 
+};
+getInfo(organisation); → 
+Name: Google 
+Partners: Microsoft Facebook */
+
+const organisation = {  
+  name: 'Google',   
+  info: { employees: ['Vlad', 'Olga'], partners: ['Microsoft', 'Facebook', 'Xing']   } 
+};
+
+function getInfo({
+  name,
+  info: {
+    partners: [$first, $second]
+  }
+}) {
+  if (!name) return "Unknown";
+  return `name: ${name} partners: ${$first} , ${$second}`;
+}
+console.log(getInfo(organisation));
+
+
+
